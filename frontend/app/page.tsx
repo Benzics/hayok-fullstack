47b18bd2400037b6ea3bbcbeb7c9be194052860f
benzics <benjaminnicholas29@gmail.com>
2024-07-20 09:41:54 +0100

    Initialized laravel 11 app for our backend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,19 @@
 import LoginForm from "@/components/LoginForm";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 export default function Home() {
   return (
-  <main className="bg-[url('../public/images/bg.jpg')] bg-no-repeat bg-cover min-h-screen flex align-middle items-center bg-gray-50">
-    <div className="container mx-auto max-w-96 py-12 px-10"
-      style={{backgroundColor: 'rgba(255, 255, 255, 0.3)'}}
-    >
-      <LoginForm />
-    </div>
-  </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="bg-[url('../public/images/bg.jpg')] bg-no-repeat bg-cover min-h-screen flex align-middle items-center bg-gray-50">
+          <div className="container mx-auto max-w-96 py-12 px-10"
+            style={{backgroundColor: 'rgba(255, 255, 255, 0.3)'}}
+          >
+            <LoginForm />
+          </div>
+      </main>
+    </QueryClientProvider>
+  
   );
 }
