bc21716ff8089ea738453f40ae4e21a1acadc151
benzics <benjaminnicholas29@gmail.com>
2024-07-25 16:36:06 +0100

    Login page ready, authentication now works and password toggle feature also added

--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
-import { FaEye } from 'react-icons/fa';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
-import { signIn } from 'next-auth/react';
+import { useAuth } from '@/hooks/auth'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import InputError from './ui/InputError';
+import { Errors } from '@/types';
 
 const LoginForm = () => {
+
+    const searchParams = useSearchParams()
+
+    const { login } = useAuth({
+        middleware: 'guest',
+        redirectIfAuthenticated: '/dashboard',
+    })
+
+    const [shouldRemember, setShouldRemember] = useState(false)
+    const [errors, setErrors] = useState<Errors>({});
+    const [status, setStatus] = useState('')
+    const [passwordVisible, setPasswordVisible] = useState(false)
+
+    const resetToken  = searchParams.get('reset')
+    useEffect(() => {
+        if (resetToken  && !errors) {
+            setStatus(atob(resetToken))
+        } else {
+            setStatus('')
+        }
+    }, [resetToken, errors])
+
   return (
     <Formik
         initialValues={{email: '', password: ''}}
@@ -13,21 +39,22 @@ const LoginForm = () => {
             password: Yup.string().required('Password must not be empty')
         })}
         onSubmit={ async (values, {setSubmitting}) => {
-           const result = await signIn('credentials', {
-            redirect: false,
-            email: values.email,
-            password: values.email
-           });
 
-           if (result?.error) {
-            console.log('Unable to login ', result.error)
-           } else {
-            console.log('Login successful');
-           }
+            const email = values.email;
+            const password = values.password;
+            const result = await login({
+                    email,
+                    password,
+                    remember: shouldRemember,
+                    setErrors,
+                    setStatus,
+                })
         }}
     >
         {({isSubmitting}) => (
             <Form>
+                
+                <InputError messages={errors.email} className="mb-2" />
                 <label
                     className="text-sm mb-2 block"
                     htmlFor="email"
@@ -52,13 +79,15 @@ const LoginForm = () => {
                     <Field
                         className="px-4 py-3 rounded block w-full outline-none text-gray-600"
                         name="password"
-                        type="password"
+                        type={passwordVisible ? 'text' : 'password'}
                         placeholder="Input password"
                     />
                     <ErrorMessage name="password" />
 
-                    <span className="absolute right-3 top-4 text-lg z-10 text-gray-500">
-                        <FaEye />
+                    <span
+                        className="absolute right-3 top-4 text-lg z-10 text-gray-500"
+                        onClick={() => setPasswordVisible(!passwordVisible)}>
+                        {!passwordVisible ? <FaEye /> : <FaEyeSlash />}
                     </span>
                 </div>
             
---
+++ frontend/types/auth.ts
@@ -0,0 +1,31 @@
+export interface AuthProps {
+    middleware?: 'guest' | 'auth';
+    redirectIfAuthenticated?: string;
+}
+
+export interface RegisterProps {
+    setErrors: React.Dispatch<React.SetStateAction<string[]>>;
+    [key: string]: any;
+}
+
+export interface LoginProps {
+    setErrors: React.Dispatch<React.SetStateAction<string[]>>;
+    setStatus: React.Dispatch<React.SetStateAction<string>>;
+    [key: string]: any;
+}
+
+export interface ForgotPasswordProps {
+    setErrors: React.Dispatch<React.SetStateAction<string[]>>;
+    setStatus: React.Dispatch<React.SetStateAction<string | null>>;
+    email: string;
+}
+
+export interface ResetPasswordProps {
+    setErrors: React.Dispatch<React.SetStateAction<string[]>>;
+    setStatus: React.Dispatch<React.SetStateAction<string | null>>;
+    [key: string]: any;
+}
+
+export interface Errors {
+    [key: string]: string[];
+}
