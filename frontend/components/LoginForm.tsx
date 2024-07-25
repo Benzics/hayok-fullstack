7ec4dc3243002aec8c0cc6ef515d48216d0995a5
benzics <benjaminnicholas29@gmail.com>
2024-07-25 08:38:17 +0100

    removed current backend implementation

--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -1,49 +1,80 @@
-"use client";
 import React from 'react'
-import LoginSubmit from './ui/LoginSubmit'
 import { FaEye } from 'react-icons/fa';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import { signIn } from 'next-auth/react';
 
 const LoginForm = () => {
   return (
-    <form>
-        <label
-            className="text-sm mb-2 block"
-            htmlFor="email"
-        >
-            Email
-        </label>
-        <input
-            className="px-4 py-3 rounded block w-full mb-5 outline-none text-gray-600"
-            name="email"
-            type="email"
-            placeholder="Email"
-        />
+    <Formik
+        initialValues={{email: '', password: ''}}
+        validationSchema={Yup.object({
+            email: Yup.string().email('Valid email required').required('email must not be empty'),
+            password: Yup.string().required('Password must not be empty')
+        })}
+        onSubmit={ async (values, {setSubmitting}) => {
+           const result = await signIn('credentials', {
+            redirect: false,
+            email: values.email,
+            password: values.email
+           });
 
-        <label
-            className="text-sm mb-2 block"
-            htmlFor="password"
-        >
-            Password
-        </label>
-        <div className="relative">
-            <input
-                className="px-4 py-3 rounded block w-full outline-none text-gray-600"
-                name="password"
-                type="password"
-                placeholder="Input password"
-            />
+           if (result?.error) {
+            console.log('Unable to login ', result.error)
+           } else {
+            console.log('Login successful');
+           }
+        }}
+    >
+        {({isSubmitting}) => (
+            <Form>
+                <label
+                    className="text-sm mb-2 block"
+                    htmlFor="email"
+                >
+                    Email
+                </label>
+                <Field
+                    className="px-4 py-3 rounded block w-full mb-5 outline-none text-gray-600"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                />
+                <ErrorMessage name="email" />
 
-            <span className="absolute right-3 top-4 text-lg z-10 text-gray-500">
-                <FaEye />
-            </span>
-        </div>
-       
+                <label
+                    className="text-sm mb-2 block"
+                    htmlFor="password"
+                >
+                    Password
+                </label>
+                <div className="relative">
+                    <Field
+                        className="px-4 py-3 rounded block w-full outline-none text-gray-600"
+                        name="password"
+                        type="password"
+                        placeholder="Input password"
+                    />
+                    <ErrorMessage name="password" />
 
-        <div className="text-right py-3">
-            <a href="#">Forgot Password?</a>
-        </div>
-        <LoginSubmit />
-    </form>
+                    <span className="absolute right-3 top-4 text-lg z-10 text-gray-500">
+                        <FaEye />
+                    </span>
+                </div>
+            
+
+                <div className="text-right py-3">
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <button
+                    className="block mt-6 w-full px-4 py-3 text-black custom-bg-lightblue rounded"
+                    type="submit"
+                >
+                    {isSubmitting? 'Signing in...' : 'Sign in'}
+                </button>
+            </Form>
+        )}
+    </Formik>
   )
 }
 
